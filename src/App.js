ddd045483891cd104b7b48c48da12706e42f525d
koskkal <61868645+koskkal@users.noreply.github.com>
2020-03-22 19:26:38 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Customers from './Components/Customers';
+import Calendar from './Components/Calendar';
+import Trainings from './Components/Trainings';
+import { BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">
+              Personal trainer schedule
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <BrowserRouter>
+        <div>
+          <Link to="/calendar">Calendar</Link> {''}
+          <Link to="/trainings">Trainings</Link> {''}
+          <Link to="/customers">Customers</Link> {''}
+          
+          <Switch>
+            <Route exact path="/calendar" component={Calendar} />
+            <Route exact path="/trainings" component={Trainings} />
+            <Route exact path="/customers" component={Customers} />
+            <Route render={() => <h1>Page not found</h1>} />
+          </Switch>
+        </div>      
+      </BrowserRouter>
+
+
     </div>
   );
 }
